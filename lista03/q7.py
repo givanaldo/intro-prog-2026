8946bf0fa92f38c94a15eadc1fd9fd1432451b0a
Givanaldo Rocha <givanaldo@gmail.com>
2026-08-25 10:30:19 -0300

    lista 03 e repetições

---
+++ lista03/q7.py
@@ -0,0 +1,10 @@
+login_padrao = "aluno"
+senha_padrao = "ifrn123"
+
+login = input("Login: ")
+senha = input("Senha: ")
+
+if login == login_padrao and senha == senha_padrao:
+    print("Acesso liberado. Bem-vindo ao SUAP!")
+else:
+    print("Credenciais inválidas. Acesso bloqueado!")
---
+++ repeticao/login.py
@@ -0,0 +1,15 @@
+login_padrao = "aluno"
+senha_padrao = "ifrn123"
+
+while
+
+
+'''
+login = input("Login: ")
+senha = input("Senha: ")
+
+if login == login_padrao and senha == senha_padrao:
+    print("Acesso liberado. Bem-vindo ao SUAP!")
+else:
+    print("Credenciais inválidas. Acesso bloqueado!")
+'''
