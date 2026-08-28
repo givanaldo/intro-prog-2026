5582d72ef1e7faa333444561e603a33c976cdd5a
Givanaldo Rocha <givanaldo@gmail.com>
2026-08-28 10:20:51 -0300

    repeticao

--- repeticao/login.py
+++ repeticao/login.py
@@ -3,12 +3,13 @@ senha_padrao = "ifrn123"
 login = ""
 senha = ""
 
-while login != login_padrao or senha != senha_padrao:
+while True:
     login = input("Login: ")
     senha = input("Senha: ")
     if login != login_padrao or senha != senha_padrao:
         print("Acesso inválido, tente novamente!")
     else:
         print("Acesso liberado!")
+        break
 
 print("Restante do código")
