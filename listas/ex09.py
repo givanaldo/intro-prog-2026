150d453eeb42b5373478eddcb9646a6d3c47c6c8
Givanaldo Rocha <givanaldo@gmail.com>
2026-09-18 10:13:46 -0300

    listas

---
+++ listas/ex09.py
@@ -0,0 +1,3 @@
+carros = ["Gol", "Palio", "Fusca", "Chevette"]
+for n, carro in enumerate(carros):
+    print(f"Carro {n+1}: {carro}")
---
+++ listas/ex10.py
@@ -0,0 +1,10 @@
+quadrados = []
+for n in range(1, 11):
+    quadrados.append(n ** 2)
+print(quadrados)
+
+quadrados = [n**2 for n in range(1, 11)]
+print(quadrados)
+
+pares = [n for n in range(2, 51, 2)]
+print(pares)
